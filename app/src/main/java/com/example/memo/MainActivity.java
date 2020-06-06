1abc6b7d9655813912b3aad5fa2d89292a48fc89
ElvanRaykwan <elvanraykwan@gmail.com>
2020-06-06 20:40:03 +0800

    A prototype of dynamic add button. Saved for save keeping

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.memo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,26 +14,46 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout relativeLayout;
+    int id = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        relativeLayout = findViewById(R.id.relativeLayout);
-        Button button = new Button(this);
-        button.setText("Add");
-        button.setLayoutParams(new
-                RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-        button.setOnClickListener(new View.OnClickListener() {
+        // get the reference of RelativeLayout
+        relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+        // set the layout params for ImageView
+        RelativeLayout.LayoutParams btnParam = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        // create a Button
+        Button myButton = new Button(this);  // create a new Button
+        myButton.setId(id);
+        id++;
+        myButton.setText("Add"); // set Text in the Button
+        myButton.setLayoutParams(btnParam); // set defined layout params to Button
+        myButton.setTextColor(Color.WHITE); // set white color for the text of Button
+        relativeLayout.addView(myButton);
+
+        // set the layout params for Button
+        RelativeLayout.LayoutParams buttonParam = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.MATCH_PARENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        Button myButton = new Button(this);  // create a new Button
+        myButton.setText("Dynamic Button2"); // set Text in the Button
+        myButton.setLayoutParams(buttonParam); // set defined layout params to Button
+        myButton.setTextColor(Color.WHITE); // set white color for the text of Button
+        myButton.setBackgroundColor(Color.parseColor("#95C03C")); // set Button's background color
+        buttonParam.addRule(RelativeLayout.BELOW, 1); // set Button to the below of ImageView
+        relativeLayout.addView(myButton); // add Button in RelativeLayout
+        // perform setOnClickListener event
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "This button is created dynamically",
-                        Toast.LENGTH_SHORT).show();
+                // display a toast on Button click
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
             }
         });
-        if (relativeLayout != null){
-            relativeLayout.addView(button);
-        }
     }
 }
 
