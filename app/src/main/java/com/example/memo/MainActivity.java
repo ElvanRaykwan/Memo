8ae188e3c07b1b8a5511a9fb50b223eb6a7b92c1
ElvanRaykwan <elvanraykwan@gmail.com>
2020-06-17 23:32:17 +0800

    Add button completed

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.memo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,40 +23,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // get the reference of RelativeLayout
         relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
-        // set the layout params for ImageView
-        RelativeLayout.LayoutParams btnParam = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT);
-        // create a Button
-        final Button myButton = new Button(this);  // create a new Button
-        myButton.setId(id);
-        id++;
-        myButton.setText("Add"); // set Text in the Button
-        myButton.setLayoutParams(btnParam); // set defined layout params to Button
-        myButton.setTextColor(Color.WHITE); // set white color for the text of Button
-        relativeLayout.addView(myButton);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // display a toast on Button click
-                Toast.makeText(getApplicationContext(), "Button "+myButton.getId()+ " Clicked", Toast.LENGTH_LONG).show();
-                addButton();
-            }
-        });
+        addButton();
     }
 
     public void addButton(){
         // set the layout params for Button
         RelativeLayout.LayoutParams buttonParam = new RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.MATCH_PARENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
         Button myButton = new Button(this);  // create a new Button
-        myButton.setText("Add"); // set Text in the Button
-        myButton.setLayoutParams(buttonParam); // set defined layout params to Button
-        myButton.setTextColor(Color.WHITE); // set white color for the text of Button
-        buttonParam.addRule(RelativeLayout.BELOW, id-1); // set Button to the below of ImageView
         myButton.setId(id);
         id++;
+        Drawable img = getResources().getDrawable(R.drawable.add_button);
+        myButton.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
+        myButton.setText("Add"); // set Text in the Button
+        myButton.setLayoutParams(buttonParam); // set defined layout params to Button
+        buttonParam.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        if(id>1) {
+            buttonParam.addRule(RelativeLayout.BELOW, id - 2); // set Button to the below of ImageView
+        }
         relativeLayout.addView(myButton); // add Button in RelativeLayout
         // perform setOnClickListener event
         myButton.setOnClickListener(new View.OnClickListener() {
