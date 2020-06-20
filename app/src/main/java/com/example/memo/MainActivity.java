d7f83b728564bad5ce170a7070574b4756e0ca56
ElvanRaykwan <elvanraykwan@gmail.com>
2020-06-20 19:37:16 +0800

    Save and Load added

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -8,13 +8,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout relativeLayout;
+    EditText editText1;
     int id = 1;
 
     @Override
@@ -32,11 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 RelativeLayout.LayoutParams.MATCH_PARENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT);
         Button myButton = new Button(this);  // create a new Button
+        myButton.setPadding(0,50,0,0);
         myButton.setId(id);
         id++;
         Drawable img = getResources().getDrawable(R.drawable.add_button);
-        myButton.setCompoundDrawablesWithIntrinsicBounds(img, null, null, null);
-        myButton.setText("Add"); // set Text in the Button
+        myButton.setCompoundDrawablesWithIntrinsicBounds(null, img,  null, null);
+        //myButton.setText("Add"); // set Text in the Button
         myButton.setLayoutParams(buttonParam); // set defined layout params to Button
         buttonParam.addRule(RelativeLayout.CENTER_HORIZONTAL);
         if(id>1) {
@@ -52,6 +61,48 @@ public class MainActivity extends AppCompatActivity {
                 addButton();
             }
         });
+    }
+
+    public void Save(String fileName) {
+        try {
+            OutputStreamWriter saveData =
+                    new OutputStreamWriter(openFileOutput(fileName, 0));
+            saveData.write(editText1.getText().toString());
+            saveData.close();
+            Toast.makeText(this, "Note Saved!", Toast.LENGTH_SHORT).show();
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, "Could not save file, Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public boolean CheckFile(String fileName){
+        File file = getBaseContext().getFileStreamPath(fileName);
+        return file.exists();
+    }
+
+    public String Load(String fileName) {
+        String fileContent = "";
+        if (CheckFile(fileName)) {
+            try {
+                InputStream loadData = openFileInput(fileName);
+                if ( loadData != null) {
+                    InputStreamReader readData = new InputStreamReader( loadData );
+                    BufferedReader buffer = new BufferedReader(readData);
+                    String tempStr;
+                    StringBuilder string = new StringBuilder();
+                    while ((tempStr = buffer.readLine()) != null) {
+                        string.append(tempStr + "\n");
+                    }
+                    loadData .close();
+                    fileContent = string.toString();
+                }
+            }
+            catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+                Toast.makeText(this, "Could not load file, Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+        return fileContent;
     }
 }
 
